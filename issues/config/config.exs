f5705f2ad3012bb46c26c26653096dd9d1d8bd48
shaunmsoria <shaunmsoria@gmail.com>
2023-04-11 21:04:51 +0000

    Elixir: sort & exercise

---
+++ issues/config/config.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+# import config depending on environment, either: dev.exs, test.exs or prod.exs
+# import_config "#{Mix.env}.exs"
+
+config :issues, github_url: "https://api.github.com"
--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -46,9 +46,36 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+
+    # |> last2(count)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  # book exercise
+  # def t_last() do
+  #   last2(["e", "d", "c", "b", "a"], 3)
+  #   |> IO.inspect()
+  # end
+
+  # def last2([], _count), do: []
+  # def last2(_list, 0), do: []
+  # def last2([head | tail], count), do: last2(tail, count - 1) ++ [head]
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 
   def decode_response({:ok, body}), do: body
